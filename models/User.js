b8710b4f8a75673038a9ad5b10dcaadc72a3c945
janellekim3 <janellekim3@gmail.com>
2017-08-16 16:17:14 +0900

    initial commit2

--- models/User.js
+++ models/User.js
@@ -5,45 +5,45 @@ var mongoose = require('mongoose');
 
 // using virtual attributes
 var schemaOptions = {
-    timestamps: true,
-    toJSON: {
-        virtuals: true
-    }
+  timestamps: true,
+  toJSON: {
+    virtuals: true
+  }
 };
-//변경
+
 // create User schema
 var userSchema = new mongoose.Schema({
-    name: String,
-    email: { type: String, unique: true },
-    password: String,
-    picture: String
+  name: String,
+  email: { type: String, unique: true},
+  password: String,
+  picture: String
 }, schemaOptions);
 
 // encrypt password
 userSchema.pre('save', function(next) {
-    var user = this;
-    if (!user.isModified('password')) { return next(); }
-    bcrypt.genSalt(10, function(err, salt) {
-        bcrypt.hash(user.password, salt, null, function(err, hash) {
-            user.password = hash;
-            next();
-        });
+  var user = this;
+  if (!user.isModified('password')) { return next(); }
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      user.password = hash;
+      next();
     });
+  });
 });
 // Checking equais password
 userSchema.methods.comparePassword = function(password, cb) {
-    bcrypt.compare(password, this.password, function(err, isMatch) {
-        cb(err, isMatch);
-    });
+  bcrypt.compare(password, this.password, function(err, isMatch) {
+    cb(err, isMatch);
+  });
 };
 
 // using virtual attributes
 userSchema.virtual('gravatar').get(function() {
-    if (!this.get('email')) {
-        return 'https://gravatar.com/avatar/?s=200&d=retro';
-    }
-    var md5 = crypto.createHash('md5').update(this.get('email')).digest('hex');
-    return 'https://gravatar.com/avatar/' + md5 + '?s=200&d=retro';
+  if (!this.get('email')) {
+    return 'https://gravatar.com/avatar/?s=200&d=retro';
+  }
+  var md5 = crypto.createHash('md5').update(this.get('email')).digest('hex');
+  return 'https://gravatar.com/avatar/' + md5 + '?s=200&d=retro';
 });
 
 var User = mongoose.model('User', userSchema);
